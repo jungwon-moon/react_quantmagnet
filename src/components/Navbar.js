fe4a02b4ece246ad4d67d5d2a0916bd8634a8ab1
jungwon-moon <jwoung0@naver.com>
2022-06-12 23:25:50 +0900

    네비게이션 바에 검색창 추가

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,152 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Navbar.css"
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChartColumn, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faChartColumn, faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useMediaQuery } from "react-responsive";
 import { Outlet, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { authUser, logoutUser } from '../store/modules/auth';
 
 
-function Menu(props) {
-  const isMobile = useMediaQuery({ maxWidth: 768 })
+// Componenets
+function Menu({ title, isMobile }) {
   return (
     <>
-      <li>{props.title}</li>
-      {isMobile &&
-        <hr></hr>
-      }
+      <li>{title}</li>
+      {isMobile && <hr></hr>}
     </>
   )
 }
 
+function Search({ refs, searchWord, onChange, onKeyPress, searchItems, searchItemsIndex }) {
+  return (
+    <div className="search_box">
+      <input
+        className="search_input"
+        value={searchWord}
+        onChange={onChange}
+        onKeyUp={onKeyPress}
+      />
+      <FontAwesomeIcon className="search_btn" icon={faMagnifyingGlass} />
+        {searchWord !== ''
+        ? <SearchItems
+          refs={refs}
+          searchItems={searchItems}
+          searchItemsIndex={searchItemsIndex}
+        />
+        : <></>
+      }
+    </div>
+  )
+}
+
+function SearchItems({ refs, searchItems, searchItemsIndex }) {
+
+  return (
+    <ul className="search_items" ref={elem => (refs.current[0] = elem)}>
+      {searchItems
+        ? searchItems.map((d, index) => (
+          <Link to={`stockdetails/${d.stcd}`} key={index}>
+            {/* {console.log(index, searchItemsIndex)} */}
+            {searchItemsIndex === index
+              ? <li className="search_item_focus">{d.stcd} {d.stnm}</li>
+              : <li className="search_item">{d.stcd} {d.stnm}</li>
+            }
+          </Link>
+        ))
+        : <></>
+      }
+    </ul>
+  )
+}
+
+// Main Components
 function Navbar() {
-  const [visible, setVisible] = useState(false);
   const isMobile = useMediaQuery({ maxWidth: 768 })
-  
+
+  const [visible, setVisible] = useState(false)
+  const [searchWord, setSearchWord] = useState('')
+  const [searchItems, setSearchItems] = useState('')
+  const [searchItemsIndex, setSearchItemsIndex] = useState(0)
+  const [maxIndex, setMaxIndex] = useState(0)
+
   const authenticated = useSelector(state => state.auth.authenticated)
-  const user = useSelector(state => state.auth.data.username)
-
+  // const user = useSelector(state => state.auth.data.username)
   const dispatch = useDispatch()
+  const refs = useRef([])
 
+
+  // useEffect 
   useEffect(() => {
     dispatch(authUser())
-  }, [])
+  }, [dispatch])
 
+  useEffect(() => {
+    if (searchWord === '') {
+      setSearchItems('')
+    } else {
+      const params = {
+        'search': searchWord
+      }
+      axios({
+        method: 'get',
+        url: '/api/kr/searchstock',
+        params: params
+      }).then(
+        res => setSearchItems([...res.data.results])
+      )
+    }
+  }, [searchWord]);
+
+  useEffect(() => {
+    setMaxIndex(searchItems.length)
+  }, [searchItems])
+
+  // Events
   const onClickLogout = () => {
     dispatch(logoutUser())
   }
-  
+
   function onToggle() {
     setVisible(!visible)
   }
+
+  function onChangeSearchWord(e) {
+    const value = e.target.value
+    setSearchWord(value)
+    if (value === '') {
+      setSearchItemsIndex(0)
+    }
+  }
+
+  function onKeyPress(e) {
+    switch (e.key) {
+      case "Enter":
+        console.log(searchItems[searchItemsIndex].stnm)
+        break;
+      case "ArrowUp":
+        if (searchItemsIndex <= 0) {
+          setSearchItemsIndex(maxIndex-1)
+        } else {
+          setSearchItemsIndex(searchItemsIndex - 1)
+        }
+        break;
+        case "ArrowDown":
+          if (searchItemsIndex >= 9) {
+            setSearchItemsIndex(0)
+          } else {
+            setSearchItemsIndex(searchItemsIndex + 1)
+          }
+        break;
+      case "Escape":
+        console.log(e.key)
+        break;
+      // no default
+    }
+  }
+  console.log(searchItemsIndex)
+
 
   return (
     <>
@@ -54,22 +161,37 @@ function Navbar() {
           (visible || !isMobile) && (
             <>
               <ul className="navbar_menu">
-                {<Link to="/strategy">
-                  <Menu title="투자전략" />
-                </Link>}
-                {<Link to="/calculator">
-                  <Menu title="계산기" />
-                </Link>}
+                <Link to="/strategy">
+                  <Menu
+                    isMobile={isMobile}
+                    title="투자전략" />
+                </Link>
+                <Link to="/calculator">
+                  <Menu
+                    isMobile={isMobile}
+                    title="계산기" />
+                </Link>
               </ul>
+
+              <Search
+                refs={refs}
+                searchWord={searchWord}
+                onChange={onChangeSearchWord}
+                onKeyPress={onKeyPress}
+                searchItems={searchItems}
+                searchItemsIndex={searchItemsIndex}
+              />
+
+
               <ul className="navbar_user">
                 {authenticated === false ?
                   <>
-                    {<Link to="/login">
+                    <Link to="/login">
                       <li>로그인</li>
-                    </Link>}
+                    </Link>
                   </>
                   : <>
-                    <a onClick={onClickLogout}>로그아웃</a>
+                    <li onClick={onClickLogout}>로그아웃</li>
                   </>
                 }
               </ul>
--- src/components/StockDetails.js
+++ src/components/StockDetails.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { comma } from '../utils/utils'
 import Chart from "react-apexcharts"
 import axios from "axios"
 import "./StockDetails.css"
@@ -15,12 +16,12 @@ const StockDetailHeader = ({ details, price }) => {
       </div>
 
       <div className="priceArea">
-        <div className="priceBox">{price.close}</div>
+        <div className="priceBox">{comma(price.close)}</div>
         <div className="prevdBox">
           <div style={{ fontWeight: "300" }}>기준일: {price.date}</div>
           {price.prevd > 0
-            ? <div style={{ color: "red" }}>+{price.prevd}(+{price.rate} %)</div>
-            : <div style={{ color: "blue" }}>{price.prevd}({price.rate} %)</div>
+            ? <div style={{ color: "red" }}>+{comma(price.prevd)}(+{price.rate} %)</div>
+            : <div style={{ color: "blue" }}>{comma(price.prevd)}({price.rate} %)</div>
           }
         </div>
       </div>
@@ -71,11 +72,11 @@ const StockDetailBody = ({ details, price, prices }) => {
       <div className="contentTable">
         <div className="contentTableRow">
           <div>전일 종가</div>
-          <div>{String(price.close - price.prevd)}</div>
+          <div>{String(comma(price.close - price.prevd))}</div>
         </div>
         <div className="contentTableRow">
           <div>시작가</div>
-          <div>{price.open}</div>
+          <div>{comma(price.open)}</div>
         </div>
         <div className="contentTableRow">
           <div>PBR</div>
@@ -86,15 +87,27 @@ const StockDetailBody = ({ details, price, prices }) => {
           <div>{details.per}</div>
         </div>
         <div className="contentTableRow">
-          <div>PER</div>
-          <div>{details.per}</div>
+          <div>BPS</div>
+          <div>{details.bps}</div>
+        </div>
+        <div className="contentTableRow">
+          <div>DPS</div>
+          <div>{details.dps}</div>
+        </div>
+        <div className="contentTableRow">
+          <div>ROE</div>
+          <div>{details.roe}</div>
+        </div>
+        <div className="contentTableRow">
+          <div>DVD_YLD</div>
+          <div>{details.dvd_yld}</div>
         </div>
       </div>
     </div>
   )
 }
 
-
+// Main Components
 const StockDetails = () => {
   const stcd = useParams().stcd
   const [price, setPrice] = useState([{  }])
@@ -102,28 +115,26 @@ const StockDetails = () => {
     date: '000000'
     }])
   const [details, setDetails] = useState([{}])
-  const params = {
-    stcd__contains: stcd,
-  }
+
 
   useEffect(() => {
     axios({
       method: 'get',
       url: '/api/kr/valuation/',
-      params: params
+      params: { stcd__contains: stcd }
     }).then(
       res => {
         const data = res.data.results
         setDetails(data[data.length - 1])
       }
     )
-  }, [])
+  }, [stcd])
 
   useEffect(() => {
     axios({
       method: 'get',
       url: '/api/kr/stockprice/',
-      params: params
+      params: { stcd__contains: stcd }
     }).then(
       res => {
         const data = res.data.results
@@ -131,7 +142,7 @@ const StockDetails = () => {
         setPrices(data.slice(-200))
       }
     )
-  }, [])
+  }, [stcd])
 
 
   return (
