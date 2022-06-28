8c2da3db7369d7d87758ac59d539a5c8dc31fa7d
jungwon-moon <jwoung0@naver.com>
2022-06-29 03:36:12 +0900

    공용 css를 Home.css에서 Common.css로 이동

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import App from './App';
-import './index.css';
+import App from './App'
+import './index.css'
+import './components/Common.css'
 
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals'
 
 import { Provider } from 'react-redux'
 import store from './store'
