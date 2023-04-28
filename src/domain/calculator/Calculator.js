99702cf31d43990277c7c664dadee5080ea1d739
jungwon-moon <jwoung0@naver.com>
2023-04-28 23:23:28 +0900

    /calculatior 에 뒤로가기 버튼 추가

--- src/domain/calculator/Calculator.js
+++ src/domain/calculator/Calculator.js
@@ -1,13 +1,30 @@
-import style from "./Calculator.module.scss"
 import React from "react"
+import { useNavigate } from "react-router-dom"
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Card from "../../components/DomainCard"
 import calculatorListJS from "../../store/json/calculatorList.json"
 
+import style from "./Calculator.module.scss"
+
 
 const Calculator = () => {
+  const navigate = useNavigate()
+
+  const onClickGoBack = () => {
+    navigate("/")
+  }
+  
   return (
     <div className={style.content}>
-      <div className={style.title}>금융 계산기</div>
+      <div className={style.title}>
+        <FontAwesomeIcon 
+          icon={faChevronLeft}
+          className={style.titleButton}
+          onClick={onClickGoBack}
+          />
+        금융 계산기
+      </div>
       {
         calculatorListJS.map((item, index) => (
           <Card key={index}
