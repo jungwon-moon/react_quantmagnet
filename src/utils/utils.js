736005b6c0741dea8da59629068621240e7748c0
jungwon-moon <jwoung0@naver.com>
2023-04-30 02:42:44 +0900

    uilts.js 수정 uncomma 함수 추가 - comma한 값을 되돌리기 위해서 getKeywordsTime 함수 삭제 - 더 이상 필요 없음

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,19 +3,14 @@ function comma(text) {
   return text.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,')
 }
 
+function uncomma(text) {
+  text = String(text)
+  return text.replace(/[^\d]+/g, '')
+}
+
 function padZero(text) {
   return String(text).padStart(2, '0')
 }
 
-function getKeywordsTime() {
-  const date = new Date()
-  const min = date.getMinutes()
-  if (min < 10) {
-    date.setHours(date.getHours() - 1)
-  }
-  date.setMinutes(10)
-  date.setHours(parseInt(date.getHours() / 3) * 3)
-  return date.getFullYear() + padZero(date.getMonth() + 1) + padZero(date.getDate()) + padZero(date.getHours()) + '05'
-}
 
-export { comma, getKeywordsTime }
+export { comma, uncomma, padZero}
