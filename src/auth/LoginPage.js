c6a31cc0fb68a5135fa388958d23f12137618f4f
jungwon-moon <jwoung0@naver.com>
2023-05-04 04:13:52 +0900

    login 페이지

--- src/auth/LoginPage.js
+++ src/auth/LoginPage.js
@@ -1,22 +1,27 @@
-import { React, useState } from "react";
-import { Link } from 'react-router-dom';
-import {
-  useDispatch
-} from "react-redux";
-import "./LoginPage.css";
-import { loginUser } from '../store/modules/auth';
+import style from "./LoginPage.module.scss"
+import { React, useState } from "react"
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch } from "react-redux"
+import { loginUser } from '../store/modules/auth'
 
 
 const LoginPage = () => {
+  const navigate = useNavigate()
+
+  const onClickGoBack = () => {
+    navigate("/")
+  }
   const dispatch = useDispatch()
   // const auth = useSelector(state => state.auth)
-    
+
   // state
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [isUsername, setIsUsername] = useState(false)
   const [isPassword, setIsPassword] = useState(false)
-    
+
   const onChangeUsername = (e) => {
     setUsername(e.currentTarget.value)
     if (e.currentTarget.value.length >= 4) {
@@ -52,25 +57,39 @@ const LoginPage = () => {
   }
 
   return (
-    <>
-      <div className="loginBox">
-      <h2>로그인</h2>
-        <label>아이디</label>
-        <input type="text" value={username}
-          onChange={onChangeUsername}
-          onKeyPress={onKeyPressLogin}/>
-        <label>비밀번호</label>
-        <input type="password" value={password}
-          onChange={onChangePassword}
-          onKeyPress={onKeyPressLogin}/>
-        <br />
-        <button onClick={onClickLogin}>로그인</button>
-        <br />
-        {<Link to="/register">
-          회원가입 페이지로 이동
-        </Link>}
+    <div className={style.content}>
+      <div className={style.title}>
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          className={style.titleButton}
+          onClick={onClickGoBack}/>
+        로그인
       </div>
-    </>
+
+      <div className={style.loginBox}>
+        <label className={style.label}>아이디</label>
+        <input className={style.input}
+          type="text" value={username}
+          placeholder="영문 소문자, 숫자"
+          onChange={onChangeUsername}
+          onKeyPress={onKeyPressLogin} />
+        <label className={style.label}>비밀번호</label>
+        <input className={style.input}
+          type="password" value={password}
+          placeholder="특수문자+영문+숫자"
+          onChange={onChangePassword}
+          onKeyPress={onKeyPressLogin} />
+        <button
+          className={style.loginButton}
+          onClick={onClickLogin}>로그인</button>
+        <div className={style.footer} >
+          {/* <>ID/PW 찾기</> */}
+          <Link className={style.footerButton} to="/register">
+            회원가입
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
