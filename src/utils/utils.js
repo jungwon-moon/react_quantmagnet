4a784003ce6a0f1a37678528ab19ff8c93eeb872
jungwon-moon <jwoung0@naver.com>
2023-05-03 22:35:27 +0900

    comma 함수 수정 숫자 앞에 0 들어가는 것을 방지

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 function comma(text) {
   text = String(text)
-  return text.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,')
+  return text.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
 function uncomma(text) {
