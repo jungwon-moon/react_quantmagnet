6bad7d17a970d823da7710432082cc25a84557e6
jungwon-moon <jwoung0@naver.com>
2023-05-06 00:06:09 +0900

    NotFound 페이지 css 수정

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,14 @@
-import React from "react"
+import style from "./NotFound.module.scss"
+import { React } from "react"
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <>
+    <div className={style.main}>
+      <Link to="" className={style.title}>Quantmagnet</Link>
       <h3>없는 페이지 입니다.</h3>
-    </>
+      <Link to="" className={style.link}>홈으로 이동</Link>
+    </div>
   )
 }
 
